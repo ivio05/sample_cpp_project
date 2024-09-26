2f22aa3e21e94f355f4e96957d354ea9f38e7a5e
ivan <ivio05@yandex.ru>
2024-09-26 23:34:09 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "my_mul.hpp"
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3) {
+        std::cerr << "Provide exactly 2 arguments";
+        
+        return 1;
+    }
+    
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+
+    std::cout << my_mul(a, b) << '\n';
+
+    return 0;
+}
